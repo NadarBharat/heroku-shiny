f7bfb8e748142e61cd07236521f7d7ae60da8d03
Bharat Muthuraman Nadar <bharat.m.nadar@gmail.com>
2021-11-29 14:12:22 +0530

    Update init.R

--- init.R
+++ init.R
@@ -3,4 +3,4 @@
 # Example R code to install packages if not already installed
 #
 
-helpers.installPackages('shiny', 'shinydashboard', 'shinyWidgets', 'shinycssloaders', 'shinyBS', 'shinyalert', 'tosca', 'LDAvis', 'servr', 'tm', 'lexRankr', 'textmineR', 'dplyr', 'DT', 'lubridate', 'readxl', 'writexl', 'tidyr', 'xfun', 'syuzhet', 'sentimentr', 'NLP', 'tidytext', 'stringr', 'stringi', ‘ggplot2')
+helpers.installPackages('shiny', 'shinydashboard', 'shinyWidgets', 'shinycssloaders', 'shinyBS', 'shinyalert', 'tosca', 'LDAvis', 'servr', 'tm', 'lexRankr', 'textmineR', 'dplyr', 'DT', 'lubridate', 'readxl', 'writexl', 'tidyr', 'xfun', 'syuzhet', 'sentimentr', 'NLP', 'tidytext', 'stringr', 'stringi', 'ggplot2')
